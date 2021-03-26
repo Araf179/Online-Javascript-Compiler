07622e64b1b653bf84238da2e625c6d1409e76d8
your name <you@example.com>
2021-03-26 16:45:06 -0400

    Updated theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import "bulmaswatch/superhero/bulmaswatch.min.css";
+import "bulmaswatch/cosmo/bulmaswatch.min.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
